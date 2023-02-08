10d40d029c5e3ddc43b5ef8d85de957110c57357
Ederson Macedo <ederson.mos@gmail.com>
2023-02-08 19:25:49 -0300

    Pratice of promise in exercise

---
+++ exercise-1/main.mjs
@@ -0,0 +1,20 @@
+import Database from './database.mjs';
+
+let database = new Database();
+Promise.all([
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)")
+]).then(() => {
+    Promise.all([
+        database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+        database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+        database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)")
+    ]).then(() => {
+        database.execute("select name, age from author").then(function(resultSelect) {
+            console.log(JSON.stringify(resultSelect, undefined,"   "));        
+        })
+    }).catch(function(e) {
+        console.log(e);
+    })  
+}).catch(function(e) {
+    console.log(e);
+})
