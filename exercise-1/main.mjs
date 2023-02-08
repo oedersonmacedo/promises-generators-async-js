2fd32e5331857703521f35fd42c47242fa47eeb0
Ederson Macedo <ederson.mos@gmail.com>
2023-02-08 19:37:31 -0300

    Refactore unified return for the error handling

--- exercise-1/main.mjs
+++ exercise-1/main.mjs
@@ -3,17 +3,15 @@ import Database from './database.mjs';
 let database = new Database();
 database.execute("create table author (id number, name string, age number, city string, state string, country string)")
 .then(() => {
-    Promise.all([
+    return Promise.all([
         database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
         database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
         database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)")
     ]).then(() => {
-        database.execute("select name, age from author").then(function(resultSelect) {
+        return database.execute("select name, age from author").then(function(resultSelect) {
             console.log(JSON.stringify(resultSelect, undefined,"   "));        
         })
-    }).catch(function(e) {
-        console.log(e);
-    })  
+    })
 }).catch(function(e) {
     console.log(e);
 })
