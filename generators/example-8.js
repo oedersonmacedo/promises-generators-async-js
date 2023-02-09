73d806641c90662f8cb3df7516a06884a9dcf528
Ederson Macedo <ederson.mos@gmail.com>
2023-02-09 15:16:37 -0300

    Example function generator calling next and to finish

---
+++ generators/example-8.js
@@ -0,0 +1,32 @@
+function* forever() {
+	let value = 1;
+	while (true) {
+        let reset = yield value++;
+        if (reset) value = 1;
+	}
+}
+
+function today() {
+	let date = new Date();
+	console.log(date);
+}
+
+const foreverGenerator = forever();
+console.log(foreverGenerator.next());
+// expect output: { value: 1, done: false }
+
+console.log(foreverGenerator.next());
+// expect output: { value: 2, done: false }
+
+console.log(foreverGenerator.next());
+// expect output: { value: 3, done: false }
+
+today();
+// expect output: print today YYYY-MM-DDTHH:mm:ss.mmmZ
+
+foreverGenerator.return();
+console.log(foreverGenerator.next(true));
+// expect output: { value: undefined, done: true }
+
+console.log(foreverGenerator.next());
+// expect output: { value: undefined, done: true }
