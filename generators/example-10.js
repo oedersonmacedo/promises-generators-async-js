6b10e5be838d493a4c22478ebb510206ecb27b8e
Ederson Macedo <ederson.mos@gmail.com>
2023-02-09 15:37:07 -0300

    added expect output

--- generators/example-10.js
+++ generators/example-10.js
@@ -13,9 +13,22 @@ function today() {
 
 const foreverGenerator = forever();
 console.log(foreverGenerator.next());
+// expect output: { value: 1, done: false }
+
 console.log(foreverGenerator.next());
+// expect output: { value: 2, done: false }
+
 console.log(foreverGenerator.next());
+// expect output: { value: 3, done: false }
+
 today();
+// expect output: print today YYYY-MM-DDTHH:mm:ss.mmmZ
+
 console.log(foreverGenerator.throw("error"));
+// generate error
+
 console.log(foreverGenerator.next(true));
+// no to print
+
 console.log(foreverGenerator.next());
+// no to print
