765ab3f1d6ab25ed28aac35a8cfceec832c1e99d
Ederson Macedo <ederson.mos@gmail.com>
2023-02-09 15:43:06 -0300

    example called in cascade

---
+++ generators/example-14.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+    return new Promise(function (resolve) {
+        setTimeout(function() {
+            resolve(a + b);
+        }, 1000);
+    });
+}
+sum(2, 2).then(function(a) {
+    sum(4, 4).then(function(b) {
+        sum(a, b).then(function(result) {
+            console.log(result);
+        });
+    });
+});
